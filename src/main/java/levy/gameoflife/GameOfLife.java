a26867b2648727fab9b3fb08d0e0c1f133dd511f
Yaffa Levy <yaffajlevy@gmail.com>
2024-09-10 17:48:05 -0400

    checkstyles

--- src/main/java/levy/gameoflife/GameOfLife.java
+++ src/main/java/levy/gameoflife/GameOfLife.java
@@ -47,7 +47,9 @@ public class GameOfLife {
         int count = 0;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) continue;
+                if (i == 0 && j == 0) {
+                    continue;
+                }
                 int newX = x + i;
                 int newY = y + j;
                 if (isInBounds(newX, newY) && grid[newX][newY] == 1) {
@@ -63,6 +65,7 @@ public class GameOfLife {
     private boolean isInBounds(int x, int y) {
         return x >= 0 && x < height && y >= 0 && y < width;
     }
+
 
     @Override
     public String toString() {
